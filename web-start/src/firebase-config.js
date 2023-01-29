a639c471bb90852a8672001ef5a59ed6012d8849
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-28 18:23:58 -0800

    Set up FriendlyChat App

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -6,9 +6,24 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyBgQByqmCXdpVfpwqrYtA7ydhRbkNzBdbk",
+  authDomain: "friendlychat-d2ed7.firebaseapp.com",
+  projectId: "friendlychat-d2ed7",
+  storageBucket: "friendlychat-d2ed7.appspot.com",
+  messagingSenderId: "880780829322",
+  appId: "1:880780829322:web:3afd2a42b135195a7b4373"
 };
+
+// Initialize Firebase
+//const app = initializeApp(config);
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
